c03967546ef39ed26d4a1cfdf06e5315c8d8a62a
Chris Dreismeier <chrisjdre@gmail.com>
2024-04-01 10:36:04 -0500

    Created Pokemon table

--- Database/pokemon_db_am.sql
+++ Database/pokemon_db_am.sql
@@ -18,3 +18,20 @@ GO
 USE [pokemon_db_am]
 GO
 
+/* pokemon table */
+
+print '' print '*** creating pokemon table'
+GO
+CREATE TABLE [dbo].[Pokemon] (
+	[PokemonID]		[int] IDENTITY(1000,1)	NOT NULL,
+	[PokemonName]		[nvarchar](50)		NOT NULL,
+	[Health]	[int]						NOT NULL,
+	[Attack]	[int]						NOT NULL,
+	[Defense]	[int]						NOT NULL,
+	[SpecialAtttack]	[int]				NOT NULL,
+	[SpecialDefense]	[int]				NOT NULL,
+	[Speed]	[int]							NOT NULL,
+	[Active]		[bit]						NOT NULL DEFAULT 1,
+	CONSTRAINT [pk_PokemonID] PRIMARY KEY([PokemonID])
+)
+GO
